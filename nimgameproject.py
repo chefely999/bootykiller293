b2cc89b00aed6288a7a5906eabced9521cbd7a35
chefely999 <chefely999@gmail.com>
2021-11-19 17:14:32 -0700

    Update nimgameproject.py

--- nimgameproject.py
+++ nimgameproject.py
@@ -1,25 +1,20 @@
-"""
-Nim Game
-
-Ely Terrazas
-
-CSC 119
-
-11/14/2021
-
-This program lets the user play the Nim game against the computer
-
+\"""
+Game of Nim from Python for Everyone 3rd edition
 """
 
 
 
-from random import seed,randint
+"""
+main() written by: Joseph Adams
+This main function is the game play driver for Game of NIM.
+"""
+from random import seed, randint
 def main():                     # definition of main program
     seed()                      # initialize the random number generator for the game play
 
     # -------------------------------------------------------
     # INPUT - to randomize the game play
-# -------------------------------------------------------
+    # -------------------------------------------------------
     ballCount = randint(10, 100)# randomly generate the amount of balls to play
     turn = randint(0,1)         # randomly generate who goes first (0) player (1) computer
     mode = randint(0,1)         # randomly generate mode of computer (0) smart (1) stupid
@@ -32,15 +27,18 @@ def main():                     # definition of main program
         print("\t\tBall count:", ballCount)
         if turn == 0:           # player turn
             print("\n\tPLAYER TURN")
-            playerTurn(ballCount)# TODO: call the playerTurn function 
+            #we call playerTurn function with ballCount argument to continue updating amount
+            ballCount= ballCount-(playerTurn(ballCount))
             turn = 1            # switch the turn 1 - for computer turn next
         else:                   # computer turn
             if mode == 0:       # computer smart mode
                 print("\n\tCOMPUTER TURN - Mode: Smart")
-                computerSmart(ballCount) # TODO: call the computerSmart function
+                #we call computerSmart with ballCount to update the count with what the smart computer takes
+                ballCount=ballCount-(computerSmart(ballCount))
             else:               # computer hard mode
                 print("\n\tCOMPUTER TURN - Mode: Stupid")
-                # TODO: call the computerStupid function
+                #computer stupid is called if it is picked and also updates ballCount whenever it picks an amount
+                ballCount=ballCount-(computerStupid(ballCount))
             turn = 0            # switch the turn 0 - for player turn next
         # TODO: ballCount needs to be updated for each turn
                 
@@ -51,87 +49,70 @@ def main():                     # definition of main program
     # -------------------------------------------------------
     print("\t\tBall count:", ballCount)
     if turn == 0 and ballCount == 0:
-        print("\n***** GAME of NIM Winner! PLAYER *****\n\n")
+        print("\n***** GAME of NIM Winner! PLAYER!You beat computer what a beast!!! *****\n\n")
     else:
-        print("\n***** Game of NIM Winner! COMPUTER *****\n\n")
+        print("\n***** Game of NIM Winner! COMPUTER!Better luck next time Player computer is really smart!!*****\n\n")
 
-def playerTurn():
-    playerTake=int(input("How many balls would you like to take?(1,2,or 3)"))
-    if playerTake==1:
-        ballCount=ballCount-1
-        print("Ball Count:",ballCount)
-        
-    if playerTake==2:
-        ballCount=ballCount-2
-        print("Ball Count:",ballCount)
-       
-        
+"""
+     this function asks the player wether they want to take 1,2, or 3 balls
+     and depending on the amount of balls taking will print if it is an approriate amount to take if not will ask them to pick an amount again.
+"""
+def playerTurn(ballCount):
+    ballsTaken= int(input("How many balls do you wish to take?(1,2, or 3)"))
+    if ballsTaken<1:
+            ballsTaken=int(input("How many balls do you wish to take?(1,2, or 3)"))
+    
+    elif ballsTaken> ballCount/2:
+        if ballCount==1:
+            ballsTaken=1
+        #if player tries to take more than half will give prompt to pick again 
+        elif ballCount==2:
+            print("You must take at least 1 ball.")
+            ballsTaken=1
+            
+        else:
+            #game will try to make it fair so player cant take all thats left and force computer to lose
+            ballsTaken= int(input("Cannot take more than half!How many balls do you wish to take?(1,2, or 3)"))
+    return ballsTaken
+
+"""
+if the computer stupid is selected the computer will take predictable amount of balls and return the updated amount of ballCount
+"""
+def computerStupid(ballCount):
+    if ballCount==1:
+        ballsTaken=1
+    #does not let amount taken to be more than half of available amount
+    elif ballCount==2:
+         ballsTaken=1
     else:
-        ballCount=ballCount-3
-        print("Ball Count:",ballCount)
-     
-    
-    
-
-def computerSmart():
-    computerTurn=randint(1,3)
-    ballCount=ballCount-computerTurn
-    return ballCount
-
-def validInput():
-    playerTake=int(input("How many balls would you like to take?(1,2,or 3)"))
-    if playerTake!=int:
-        print("Please enter a valid number 1,2, or 3")
+        ballsTaken= randint(1, ballCount//2)
+    return ballsTaken
+"""
+computer smart function takes the initial amount of balls and figures out the best way to get the ball count down to zero with more complicated math than computer stupid
+"""
+def computerSmart(ballCount):
+    if ballCount>63:
+        #smart computer checks amount beginning with
+        ballsTaken=ballCount-63
+    elif ballCount>31 and ballCount<63:
+        ballsTaken= ballCount-31
+    elif ballCount>15 and ballCount<31:
+        ballsTaken=ballCount-15
+    elif ballCount>7 and ballCount <15:
+        ballsTaken=ballCount-7
+    elif ballCount>3 and ballCount<7:
+        ballsTaken=ballCount-3
+        #once ballCount has reached a low amount the smart computer function will see how many are left and take just the right amount so the player is forced to be the one with no balls left and lose
     else:
-        computerTakeBall()
-def playAgain():
-    if ballCount==0:
-        print("Would you like to play again?(Y/N)")
-    if len.upper=="Y":
-        main()
-    else:
-        print("Have a nice day!Thank You for playing!")
+        if ballCount==1:
+            ballsTaken=1
+        elif ballCount==2:
+            ballsTaken=1
+        else:
+            ballsTaken= randint(1, ballCount//2)
+    return ballsTaken
     
-        
-        
-
-main()          # execution of main program
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
     
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-        
+main()    
